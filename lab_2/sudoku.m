fdb1ce7e68c3349465b5404a35bf790d23e69b57
Casper Andersson <andersson.casper99@gmail.com>
2021-02-01 13:45:47 +0100

    Minor optimization and cleanup

--- lab_2/sudoku.m
+++ lab_2/sudoku.m
@@ -2,66 +2,67 @@ tic
 Run()
 toc
 
-function [resmat, fail] = Solve(matrix, i, j)
+function [resmat, valid] = Solve(matrix, i, j)
     if i > 9
         i = 1;
         j = j + 1;
     end
-    if j > 9 % If j goes out of bounds we have solved it.
+    if j > 9 % If j goes out of bounds we have filled all squares
         resmat = matrix;
-        fail = false;
+        valid = true;
         return;
     end
     
     % Already has a number
     if matrix(i,j) ~= 0
-        [resmat, fail] = Solve(matrix, i + 1, j);
+        [resmat, valid] = Solve(matrix, i + 1, j);
         return;
     end
     
     % Try numbers
-    resmat = matrix;
-
-    fail = true;
+    valid = false;
     for x = 1:9
         matrix(i,j) = x;
         if isValid(matrix, i,j)
-            [resmat, fail] = Solve(matrix, i + 1, j);
+            [resmat, valid] = Solve(matrix, i + 1, j);
         end
-        if fail == false
-            return;
+        if valid == true
+            return; % Solution found
         end
     end
+    resmat = matrix;
 end
 
-function res = isValid(matrix, i, j)
+function valid = isValid(matrix, i, j)
     x = matrix(i,j);
     for row = 1:9
-        if row ~= i && matrix(row,j) == x
-            res = false;
+        if matrix(row,j) == x && row ~= i
+            valid = false;
             return;
         end 
     end
     
     for col = 1:9
-        if col ~= j && matrix(i,col) == x
-            res = false;
+        if matrix(i,col) == x && col ~= j
+            valid = false;
             return;
         end
     end
     
-    sI = floor((i-1)/3)*3 + 1;
+    % Upper left corner of the current square.
+    sI = floor((i-1)/3)*3 + 1; 
     sJ = floor((j-1)/3)*3 + 1;
     
+    % Check square
     for si = sI:sI+2
         for sj = sJ:sJ+2
-            if si ~= i && sj ~= j && matrix(si,sj) == x
-                res = false;
+            if matrix(si,sj) == x && si ~= i && sj ~= j
+                valid = false;
                 return;
             end
         end
     end
-    res = true;
+    valid = true;
 end
 
 function Sudokus = Run()
@@ -73,7 +74,7 @@ function Sudokus = Run()
         for j = 1:9
             sudoku{j} = fgetl(fid);
         end
-        fgetl(fid);
+        fgetl(fid); % Read blank line
         sudoku2 = zeros(9,9);
         for x = 1:9
             for y = 1:9
@@ -83,7 +84,8 @@ function Sudokus = Run()
         Sudokus(i) = {sudoku2};
     end
     % Sudokus is now an array of matrices (for the 10 sudokus)
-%     res = Sudokus;
+    
+    % Solve the sudokus
     for i = 1:10
         [Sudokus{i}, ~] = Solve(Sudokus{i}, 1, 1);
     end
