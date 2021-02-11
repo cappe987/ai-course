e3adb4ff0a27c0e0da86a299db1841b36816c774
Casper Andersson <andersson.casper99@gmail.com>
2021-02-11 18:46:20 +0100

    Cleanup and comments

--- lab_3/genetic_algorithm.m
+++ lab_3/genetic_algorithm.m
@@ -14,86 +14,29 @@
 % Let E(X_i, X_j) be the euclidean distance between city i and j.
 % Minimize sum_{1}^{n-1}{E(X_i, X_{i+1)}
 
-% tic
-% res = Run();
-% fitness(res{1})
-% toc
-% plot([res{1}.X], [res{1}.Y], '-o');
-
-% case1.RNG = 1;
-% case1.PS = 500;
-% case1.GEN = 500;
-% case1.CR = 0.8;
-% case1.MR = 0.01;
-% case1.k = 10;
-
-c.RNG = 2;
-c.PS = 1000;
-c.GEN = 200;
-c.CR = 0.8;
-c.MR = 0.01;
-c.k = 10;
-% cs = [case2 case3 case4 case5];
-% cs = [c];
-% function Run(Case, RNG, PS, GEN, CR, MR, k)
-
-% for i = 1:length(cs)
-%     c = cs(i);
-[pop, gens] = Run(1, c.RNG, c.PS, c.GEN, c.CR, c.MR, c.k);
+RNG = 2;
+PS = 1000;
+GEN = 200;
+CR = 0.8;
+MR = 0.01;
+k = 10;
+[pop, gens] = Run(1, RNG, PS, GEN, CR, MR, k);
 plot(gens);
 % nodes = createNodes(52);
 % plot([nodes(pop(1,:)).X], [nodes(pop(1,:)).Y], '-o');
 
-% end
-
-% temp = res;
-
-% new = cell(50,2);
-
-% for i = 1:50
-%     new(i,1) = temp(i);
-%     new(i,2) = {fitness(temp{i})};
-% end
-% sorted = sortrows(new, 2);
-
 function [population, bestPerGen] = generation(population, PS, genlimit, CR, MR, k, nodecount, distmat)
 selectedcount = PS/k;
 selected = zeros(selectedcount, nodecount);
-% fitnessArr = zeros(1,PS);
 gen = 1;
 bestPerGen = zeros(1,genlimit);
-while genlimit ~= 0
-%     % Roulette selection
-%     sum = 0;
-%     best = Inf;
-%     for i = 1:PS
-%         val = fitness(population{i});
-%         sum = sum + val;
-%         fitnessArr(i) = val;
-%         if val < best
-%             best = val;
-%             bestIndividual = population{i};
-%         end
-%     end
-% 
-%     for i = 1:PS
-%         fitnessArr(i) = fitnessArr(i)/sum;
-%     end
-    
-%     for i = 1:PS
-%         population{i} = pickOne(population, fitnessArr);
-%     end
-    
-    
-%     Tournament Selection
-%     population = population(randperm(PS, PS), :); % Randomize order.
-
-    best = Inf;
-%     partition = reshape(population, k, []); % Partition into groups
+while gen <= genlimit
+%   Tournament Selection
+    best = Inf; % Best in current generation
     rowsToTake = randperm(PS, PS); % Generate a permutation for indices
     rowIdx = 1;
     for i = 1:PS/k
-        currBest = Inf;
+        currBest = Inf; % Best in current tournament
         for j = 1:k
             idx = rowsToTake(rowIdx);
             value = fitness(population(idx, :), distmat);
@@ -110,60 +53,42 @@ while genlimit ~= 0
         end
     end
     bestPerGen(gen) = best;
-    gen = gen + 1;
-
         
     % Crossover
-%     population = cell(1,PS);
-    idx = 1;
-    for i = 1:PS
+    for i = 1:PS 
+        % Randomly select two parents from tournament winners
         p1 = selected(randsample(selectedcount, 1), :);
         p2 = selected(randsample(selectedcount, 1), :);
-%         p1 = pickOne(population, fitnessArr);
-%         p2 = pickOne(population, fitnessArr);
         if rand() < CR
+            % Select range for crossover slice
             from = randsample(nodecount-1, 1)+1;
             to   = randsample(nodecount-1, 1)+1;
-            if from > to
+            if from > to % Swap in case from is greater than to
                 temp = to;
                 to = from;
                 from = temp;
             end
             child1 = crossover(p1, p2, from, to, nodecount);
-%             child2 = crossover(p2, p1, from, to, nodecount);
             population(i,:) = child1;
-%             population{idx} = child1;
-%             population{idx + 1} = child2;
         else
-            population(i,:) = p1;
-%             population{idx} = p1;
-%             population{idx + 1} = p2;
+            population(i,:) = p1; % No crossover done
         end
-%         idx = idx + 2;
     end
 
     
     % Mutation
+    % Flip a slice in the order
     for i = 1:PS
         for j = 1:nodecount
             if rand() < MR
-                i1 = randsample(nodecount-1, 1) + 1;
-                i2 = randsample(nodecount-1, 1) + 1;
-%                 if i1 == nodecount-1
-%                     i2 = i1-1;
-%                 else
-%                     i2 = i1+1;
-%                 end
-                if i2 < i1
-                    temp = i1;
-                    i1 = i2;
-                    i2 = temp;
+                from = randsample(nodecount-1, 1) + 1;
+                to = randsample(nodecount-1, 1) + 1;
+                if to < from % Swap in case from is greater than to
+                    temp = from;
+                    from = to;
+                    to = temp;
                 end
-                population(i,i1:i2) = flip(population(i,i1:i2));
-                  
-%                 temp = population(i,i1);
-%                 population(i,i1) = population(i,i2);
-%                 population(i,i2) = temp;
+                population(i,from:to) = flip(population(i,from:to));
             end
         end
     end
@@ -171,81 +96,30 @@ while genlimit ~= 0
     
     % Replacement
     % Using best individual that we calculated earlier.
-%     population{1} = partition{bestIndex(1), bestIndex(2)};
     population(1,:) = bestIndividual;
-    genlimit = genlimit - 1;
-%     res = generation(new_population, PS, genlimit - 1, CR, MR, k, nodecount);
+    gen = gen + 1;
 end % While Loop
 
-end
-
-function res = pickOne(pop, probabilities)
-   idx = 1;
-   r = rand();
-   while r > 0
-       r = r - probabilities(idx);
-       idx = idx + 1;
-   end
-   res = pop{idx - 1};
 end
 
 function child = crossover(p1, p2, from, to, nodecount)
     child = zeros(1,nodecount);  
     child(1) = 1; % Set city 1 first
-    taken = p1(from:to);
+    taken = p1(from:to); % Slice from p1
     [~, ia] = setdiff(p2, [1 taken], 'stable');
-    p2noP1 = p2(ia);
+    p2noP1 = p2(ia); % p2 without the elements taken from p1
     
-    child(2:from-1) = p2noP1(1:from-2);
-    child(from:to) = taken; 
-%     if to ~= nodecount
-    
-
-    child(to+1:nodecount) = p2noP1(from-1:end);
-%     end
-%     child(nodecount) = p1(1); % Set city 1 last.
-end
-
-function child = crossover2(p1, p2, from, to, nodecount)
-    % Old crossover method. New version is a bit faster and much shorter.
-    child = Node.empty(0,nodecount);  
-    taken = zeros(1, to-from+1);
-    for i = from:to
-        taken(i) = p1(i).ID;
-    end
-    p2idx = 1;
-    i = 1;
-    while i < from
-       if ~ismember(p2(p2idx).ID, taken)
-           child(i) = p2(p2idx);
-           p2idx = p2idx + 1;
-           i = i + 1;
-       else
-           p2idx = p2idx + 1;
-       end
-    end
-    for i = from:to
-        child(i) = p1(i);
-    end
-    
-    i = to + 1;
-    while p2idx <= nodecount
-       if ~ismember(p2(p2idx).ID, taken)
-           child(i) = p2(p2idx);
-           p2idx = p2idx + 1;
-           i = i + 1;
-       else
-           p2idx = p2idx + 1;
-       end
-    end
+    child(2:from-1) = p2noP1(1:from-2); % Insert from p2
+    child(from:to) = taken; % Insert slice from p1
+    child(to+1:nodecount) = p2noP1(from-1:end); % Insert rest of p2
 end
 
 function value = fitness(indv, distmat)
     value = 0;
-    for i = 1:length(indv)-1
-        value = value + distmat(indv(i), indv(i+1));
+    for i = 1:length(indv)-1 % All cities
+        value = value + distmat(indv(i), indv(i+1)); 
     end
-    value = value + distmat(1, indv(length(indv))); % Back to start
+    value = value + distmat(1, indv(length(indv))); % Last city -> start
 end
 
 function nodes = createNodes(nodecount)
@@ -272,15 +146,12 @@ function [population, gens] = Run(Case, RNG, PS, GEN, CR, MR, k)
     
     rng(RNG);
     
-%     initial = cell(1,PS);
     initial = zeros(PS,52);
     for i = 1:PS
         initial(i,1) = 1;
         initial(i,2:nodecount) = randperm(nodecount-1, nodecount-1)+1;
-%         n(nodecount+1) = nodes(1);
-%         initial{i} = n;
     end
-%     res = initial;
+
     tic
     [population, gens] = generation(initial, PS, GEN, CR, MR, k, nodecount, distmat);
     disp(strcat("--- Case ", num2str(Case), " ---"));
@@ -290,12 +161,8 @@ function [population, gens] = Run(Case, RNG, PS, GEN, CR, MR, k)
     disp(strcat("CR: ", num2str(CR)));
     disp(strcat("MR: ", num2str(MR)));
     disp(strcat("k: ", num2str(k)));
-    disp(fitness(population(1,:), distmat))
+    disp(strcat("Fitness: ", num2str(fitness(population(1,:), distmat))));
     toc
-%     res = population; % fitness(population{1});
-%     for i = 1:nodecount+1
-%         disp(res{1,1}(1,i).ID);
-%     end
 end
 
 
