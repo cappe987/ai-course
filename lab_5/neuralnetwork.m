756c6d90dd533c34477c00710070a07c43deacab
Casper Andersson <andersson.casper99@gmail.com>
2021-03-03 18:19:13 +0100

    Passed 80% once.

--- lab_5/neuralnetwork.m
+++ lab_5/neuralnetwork.m
@@ -1,43 +1,84 @@
 % clear all
 
-
+% [testAcc, perClass] = test(layers, bestW, B, weightCount, inputs, targets, 800:n);
+% fprintf("Test %d: %.1f%%\n", i, testAcc);
+% for i = 1:10
+%     fprintf("Percentage correct for %d: %.1f\n", i-1, perClass(i));
+% end
+% return;
 
 rng(1);
 
 layers = [784 200 100 10]; % input - hidden layers - output
-eta = 0.2; % Learning rate
+eta = 0.1; % Learning rate
 % inputs = [1 3 1; 2 3 2];
 % targets = [1 2 1];
-L = cell(1, length(layers)); % L{1} is input. L{end} is output
 
 weightCount = length(layers)-1;
 
+% matrix = loadMatrix();
 
 tic
-
-% matrix = readmatrix('assignment5.csv');
-n = 5000;
+n = 1000; % 5000
 inputs = transpose(matrix(1:n, 2:end));
 targets = matrix(1:n,1);
 
 
-[W,B] = initWB(layers, weightCount);
-W = train(layers, weightCount, eta, L, W, B, inputs, targets);
+[W,B, tAccs, vAccs] = doTraining(layers, weightCount, eta, inputs, targets, n);
 
-L = runNetwork(L,W,B, weightCount, inputs(:,14)); % 41: 9
+% L = runNetwork(layers, W, B, weightCount, inputs(:,14)); % 41: 9
 % disp(outputToNum(L{end}));
-disp(topThree(L{end}));
+% disp(topThree(L{end}));
 
 toc
 
+function matrix = loadMatrix()
+    disp("Reading CSV");
+    matrix = readmatrix('assignment5.csv');
+    disp("CSV Loaded");
+end
 
-function W = train(layers, weightCount, eta, L, W, B, inputs, targets)
-    for curr = 1:length(inputs(1,:))
+function [W,B, trainingAccs, validationAccs] = doTraining(layers, weightCount, eta, inputs, targets, n)
+    disp("Training...");
+    [W,B] = initWB(layers, weightCount);
+    tenth = floor(n/10);
+    validationRange = (tenth*7):(tenth*8 - 1);
+    testStart = tenth*8;
+    i = 1;
+    j = 3;
+    while j > 0
+        [W, trainAcc] = train(layers, weightCount, eta, W, B, inputs, targets, n);
+        acc = validate(layers, W, B, weightCount, inputs, targets, validationRange);
+        fprintf("---- Iteration %d ----\n", i);
+        fprintf("Training   : %.1f%%\n", trainAcc);
+        fprintf("Validation : %.1f%%\n", acc);
+        trainingAccs(i) = trainAcc;
+        validationAccs(i) = acc;
+        i = i + 1;
+        if acc > 80
+            [testAcc, perClass] = test(layers, W, B, weightCount, inputs, targets, testStart:n);
+            fprintf("Test: %.1f%%\n", testAcc);
+            for i = 1:10
+                fprintf("Percentage correct for %d: %.1f%%\n", i-1, perClass(i));
+            end
+            j = j - 1;
+        end
+    end
+    disp("Training done");
+end
+
+function [W, acc] = train(layers, weightCount, eta, W, B, inputs, targets, n)
+    tenth = floor(n/10);
+    trainEnd = tenth*7 - 1;
+    results = zeros(1,trainEnd);
+
+    for curr = 1:trainEnd
         input = inputs(:, curr);
         target = createTargetOutput(targets(curr));
         
         % Run neural network
-        L = runNetwork(L, W, B, weightCount, input);
+        L = runNetwork(layers, W, B, weightCount, input);
+        results(curr) = outputToNum(L{end}) == targets(curr);
 %         disp(L{end});
 
         % Backpropagation
@@ -53,14 +94,14 @@ function W = train(layers, weightCount, eta, L, W, B, inputs, targets)
 
         for lay = flip(1:weightCount)
             % Find Weight deltas
-            for i = 1:layers(lay) % Parfor?
+            for i = 1:layers(lay) 
                 for j = 1:layers(lay+1)
                     weightDeltas{lay}(j,i) = eta * delta(j) * L{lay}(i);
                 end
             end
             % Calculate all delta J for layer i
             newDeltas = zeros(1,layers(lay));
-            for j = 1:layers(lay) % Parfor?
+            for j = 1:layers(lay)
                 derivOutput = derivsigmoid(L{lay}(j));
                 total = 0;
                 for k = 1:layers(lay+1)
@@ -73,25 +114,77 @@ function W = train(layers, weightCount, eta, L, W, B, inputs, targets)
 
 
         % Apply weight deltas
-        for i = 1:weightCount % Parfor?
+        for i = 1:weightCount
             W{i} = W{i} + weightDeltas{i};
         end
+        
+%         % Run validation
+%         if mod(curr, 100) == 0
+%             acc = validate(layers, W, B, weightCount, inputs, targets, validationRange);
+%             fprintf("Validation after %d images: %.1f%%\n", curr, acc);
+%         end
     end
+    
+    acc = (sum(results)/length(results))*100;
 end
 
 
-function L = runNetwork(L, W, B, weightCount, input)
+function L = runNetwork(layers, W, B, weightCount, input)
+    L = cell(1, length(layers)); % L{1} is input. L{end} is output
     L{1} = arrayfun(@(x) sigmoid(x), input);%Input. Do we use sigmoid on input?
-    % L{2} = W{1}*L{1} + 1;
     for i = 1:weightCount-1
-    %     A = arrayfun(@(x) sigmoid(x), L{i});
-    %     L{i+1} = W{i}*A + B{i};
         A = W{i}*L{i} + B{i}; % Linear regression with multiple variables
         L{i+1} = arrayfun(@(x) sigmoid(x), A); 
     end
 
     L{end} = softmax(W{weightCount}*L{weightCount} + 1);
+end
 
+function accuracy = validate(layers, W, B, weightCount, inputs, targets, idxRange)
+%     len = length(idxRange);
+    results = zeros(1, length(idxRange));
+    diff = idxRange(1)-1;
+    for curr = idxRange
+        input = inputs(:, curr);
+        target = targets(curr);
+
+        % Run neural network
+        L = runNetwork(layers, W, B, weightCount, input);
+        res = outputToNum(L{end});
+        if res == target
+            results(curr-diff) = 1;
+        else
+            results(curr-diff) = 0;
+        end
+    end
+    accuracy = (sum(results)/length(results)) * 100;
+end
+
+function [accuracy, perClass] = test(layers, W, B, weightCount, inputs, targets, idxRange)
+    results = zeros(1, length(idxRange));
+    diff = idxRange(1)-1;
+    correct = zeros(1,10);
+    total   = zeros(1,10);
+    for curr = idxRange
+        input = inputs(:, curr);
+        target = targets(curr);
+
+        % Run neural network
+        L = runNetwork(layers, W, B, weightCount, input);
+        res = outputToNum(L{end});
+        total(target+1) = total(target+1) + 1;
+        if res == target
+            correct(target+1) = correct(target+1) + 1;
+            results(curr-diff) = 1;
+        else
+            results(curr-diff) = 0;
+        end
+    end
+    perClass = zeros(1,10);
+    for i = 1:10
+        perClass(i) = (correct(i)/total(i))*100;
+    end
+    accuracy = (sum(results)/length(results)) * 100;
 end
 
 function [W,B] = initWB(layers, weightCount)
